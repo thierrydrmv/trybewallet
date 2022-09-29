2c51f2780698d21048f72d59e6efe644029526d9
Thierry <varelathierry@gmail.com>
2022-09-29 20:44:38 -0300

    requisito 2 header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import WalletForm from '../components/WalletForm';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <WalletForm />
       </div>
     );
