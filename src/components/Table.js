773a37f0e8abd019370ed187712d807b235e500b
Thierry <varelathierry@gmail.com>
2022-10-07 00:13:17 -0300

    requisito 6 tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
     return (
-      <div>Table</div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.objectOf,
+  ).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
